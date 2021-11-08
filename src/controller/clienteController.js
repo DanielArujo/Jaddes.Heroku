acc927c93cadf97092d80af757738daf019b79a7
DanielAraujo <daniel.araujos22004@gmail.com>
2021-11-08 00:29:26 -0300

    cadastro

--- src/controller/clienteController.js
+++ src/controller/clienteController.js
@@ -16,4 +16,27 @@ app.get('/', async(req, resp) => {
 })
 
 
+app.post('/', async(req, resp) => {
+    try{
+
+        let { nome, telefone, email, senha, endereco, numeroEndereco, complemento } = req.body;
+
+        let r = await db.infoc_jdf_cliente.create({
+            nm_cliente: nome,
+            nr_telefone: telefone,
+            ds_email: email,
+            ds_senhaEmail: senha,
+            ds_endereco: endereco,
+            nr_endereco: numeroEndereco,
+            ds_complemento: complemento
+        });
+        resp.send(r);
+
+    }catch (e){
+        resp.send(e.toString())
+    }
+
+})
+
+
 export default app;
