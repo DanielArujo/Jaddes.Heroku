6fda6845c54f7e517efa0fd2eca7b9c443aacbff
DanielAraujo <daniel.araujos22004@gmail.com>
2021-11-16 08:22:00 -0300

    pedidoss

--- src/controller/pedidosController.js
+++ src/controller/pedidosController.js
@@ -19,23 +19,78 @@ app.get('/', async(req, resp) => {
 
 app.post('/', async(req, resp) => {
 
-    let {nome} = req.body
-
-
     try{
-        let r = await db.infoc_jdf_pedido.create({
-            nm_cliente: nome
-        },
-        {
-            
-        });
-        resp.send(r);
+
+        let {idproduto, idcliente, formaPagamento, status } = req.body
+
+
+        const usuario = await db.infoc_jdf_cliente.findOne({
+            where:{id_cliente: idcliente}
+       })
+
+        const pedido = await db.infoc_jdf_pedido.create({
+            id_cliente: usuario.id_cliente,
+            ds_formaPagamento: formaPagamento,
+            ds_status: status
+        })
+
+        const produto = await db.infoc_jdf_produto.findOne({
+            where:{id_produto: idproduto}
+        })
+
+        const pedidoItem = await db.infoc_jdf_item_pedido.create({
+            id_pedido: pedido.id_pedido,
+            id_produto: produto.id_produto
+        })
+
+        resp.sendStatus(200);
 
     }catch (e){
         resp.send(e.toString())
     }
 })
 
+
+app.post('/v2', async(req, resp) => {
+
+    try{
+
+        const {formaPagamento, status, produtos , idcliente} = req.body; 
+
+
+        const usuario = await db.infoc_jdf_cliente.findOne({
+            where:{id_cliente: idcliente}
+       })
+
+       
+
+        for(var product of produtos){
+            const {idproduto} = product;
+
+            const pedido = await db.infoc_jdf_pedido.create({
+                id_cliente: usuario.id_cliente,
+                ds_formaPagamento: formaPagamento,
+                ds_status: status
+            })
+
+            const produto = await db.infoc_jdf_produto.findOne({
+                where:{id_produto: idproduto}
+            })
+    
+            const pedidoItem = await db.infoc_jdf_item_pedido.create({
+                id_pedido: pedido.id_pedido,
+                id_produto: produto.id_produto
+            })
+    
+        }
+
+        
+        resp.sendStatus(200);
+
+    }catch (e){
+        resp.send(e.toString())
+    }
+})
 
 
 
